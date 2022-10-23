3eac5c814f94dd40dd81353108877855f88851ee
Natallia Samoilik <samoiliknata@gmail.com>
2022-10-23 14:05:39 +0200

    added log!

--- src/main/java/Triangle.java
+++ src/main/java/Triangle.java
@@ -1,7 +1,13 @@
-public class Triangle {
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+public class Triangle {
     public static double areaOfTriangle(double a, double b, double c) {
+
+        log.info("double a: " + a + ", double b: " + b + ", double c: " + c);
         double perimeter = (a + b + c) / 2;
-        return Math.sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c));
+        double square = Math.sqrt(perimeter * (perimeter - a) * (perimeter - b) * (perimeter - c));
+        log.info("square of triangle is: " + square);
+        return square;
     }
 }
